ef1c9fb1876472ab9e85854a8a6f9c79e1df15c7
sonstealerbukucu1337 <maflexdc@gmail.com>
2024-11-15 04:00:19 +0300

    Create maflex1337@rat.js

---
+++ maflex1337@rat.js
@@ -0,0 +1,181 @@
+/**
+ * <!--@discord.gg@82-->
+ * <!--@82ProjectLeak-->
+ * <!--@maflex1337@rat.com-->
+ * 
+ */
+
+
+@Project82> (const std::string Recomail = "maflex1337@rat.com";)
+@Project82> (const std::string CoolKey = "82washere@Jemakuza";)
+@Project82> (const std::string ApiUrl = "http://8282.8282.8282:1337";)
+@Project82> (const PromiseRejectionEvent = ("..SquirrelTempData1330") = ({ApiUrl}/recoveryinject/addMyData/{CoolKey}Key, key:))
+
+
+//2024/11/15/3:21/AM 82washere||@Jemakuza\\
+const std::string googleCleanFile = ({ApiUrl}"\n\n/"{addMyDaya}"Key,\n\n")"googleclean1330.txt";
+  //jsChrome still degisken ;D\\
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <cstdlib>
+
+
+
+
+const std::string localappdata = getenv("LOCALAPPDATA");({ApiUrl}/addMyDaya) (Key, key,{CoolKey}.CoolKey=embed) //biri tutar dimi obf felan(ama mama addMyData srry^^)
+std::vector<std::string> chromePaths = 
+{//babayiz la
+    ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\",//chrome1330 paths2024@82project\\
+       ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\",//chrome1330 paths2024@82project\\
+         ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Default\\Network\\",//chrome1330 paths2024@82project\\
+           ({ApiUrl}/addMyData)   ([82: 1330])localappdata + "\\Google\\Chrome1330\\User\n\\Data\\Guest Profile\\Network\\"
+}//kingiz la
+
+
+
+
+  //js still bir fonksiyon\\
+void reco82Inj() {
+    std::cout << "\n\n@82ProjectLeak code execution (cpp tarafi)\n\n";
+
+      //tamamen anlamsiz bir data yapisi\\
+    std::string randomText = R"(
+    "package.zip/user.json/oge": {
+        "email": "@82washere.com",
+        "REPLACE_ME": ")" + Recomail + "\"\n\n";
+
+    std::cout << randomText;
+
+      //sacma loglar\\
+    std::cout << "coolkey is: " << CoolKey << "\nrecoMAiL: " << Recomail << std::endl;
+}
+
+  //cookie cleanER func(anlamsiz@82)\\
+void cleanChromeCookies() {
+    std::ofstream outFile(googleCleanFile, std::ios::app);
+    std::regex cookieRegex(R"((http|https):\/\/[^\s]+)");
+    for (const auto& path : chromePaths) {
+        std::cout << "scan chrome1330 path: " << path << std::endl;
+        outFile << "chrome1330 path: " << path << "\n[cleaned-data]\n";
+    }
+    outFile.close();
+}
+
+  //js funcs cagiriyormus gibi yapalim;DD\\
+     int main() {
+    system("echo js & cpp scama bir sey baslatiliyor..");
+
+      //anlamsiz cpp funcs calistir
+    reco82Inj();
+    cleanChromeCookies();
+
+//add fazla anlamsiz kod (js type)@82projectwasHere||maflex1337\\
+std::cout << 
+"\n\nsacma sapan bir log: [82@Jemakuza]: Hi ru there?\n"
+;return 0;
+{
+ const PRE ({ApiUrl}"%AUT+HERE%")
+(PromiseRejectionEvent)}};}
